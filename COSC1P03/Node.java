b9845815781e2b7f0bbcb6e24d1cf765336f8433
Maisam A <45243399+Koorikdat@users.noreply.github.com>
2021-12-19 14:46:45 -0500

    Delete Node.java

--- COSC1P03/Node.java
+++
@@ -1,8 +0,0 @@
-public class Node {
-    public int item;//need to be public
-    public Node next;//need to be public
-    public Node (int numberPassed, Node nextNodePassed) {
-        item = numberPassed;
-        next = nextNodePassed;
-    }
-}
